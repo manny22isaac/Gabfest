136334f725d9195b27ea0eb1d63dc5a979befa02
manny22isaac <34252065+manny22isaac@users.noreply.github.com>
2019-08-27 00:37:28 -0400

    Functions for Gabfest.
    Added:
    - An __init__ function to store variables and pass them into another function to call on a users name age filters under a class.
    - Two inputs for name age and which subreddits to select from a list.
    - A while loop and a for loop to select a group of subreddits topics and to print out the available options.

--- askreddit.py
+++ askreddit.py
@@ -1,47 +1,100 @@
 #/Users/ammanuelisaac/Desktop/Gabfest/askreddit
 
-import praw, random, pprint
-#line4-7 are what allow a user to send a client request to reddit's servers.
-#to use the API you need to have a reddit account.
-#line 8 will return a Boolean Value if the request came through.
+import praw, random, time, sys
 
 reddit = praw.Reddit(client_id='',
                              client_secret='',
                              user_agent='')
 print(reddit.read_only)
-#the amount of subreddits.
-subreddits = ['AskReddit',\
-                      'AskMen',\
-                      'AskWomen']
 
-submissions = ['controversial',\
-                  'new',\
-                  'top',\
-                  'gilded',\
-                  'hot',\
-                  'rising']
 
-#if a title has a curse word in the title the program will pass it in line 39
-curseWords = []
-
-#this list will also pass questions in askReddit form
-redditFormatQuestions = ['AskReddit', 'askreddit']
-
-#selects a random subreddit from the subreddit list
 secure_Random = random.SystemRandom()
 
+#--------------------------------------------------------#
+    #selects a random subreddit from the subreddit list
+secure_Random = random.SystemRandom()
 
-#this will select one of the string variables at random
-#then it will make the call the the server with the variable reddit as the client id
+#--------------------------------------------------------#
+    #the amount of subreddits.
+subreddits = ['AskReddit',\
+                          'AskMen',\
+                          'AskWomen']
+all_subreddits = []
+#--------------------------------------------------------#
+#this lists are used in the lines 52-67
+submissions = ['controversial',\
+                      'new',\
+                      'top',\
+                      'gilded',\
+                      'hot',\
+                      'rising']
+curseWords = ['sex',\
+                  'NSFW',\
+                  'fuck',\
+                  'shit',\
+                  'ass',\
+                  'dick',\
+                  'cunt',]
 
-age = int(input("Please enter your age. --> "))
-if age < 18:
-    for submission in reddit.subreddit(secure_Random.choice(subreddits)).new(limit=5):
-        if str(curseWords) in submission.title:
-            pass
-        pprint.pprint(submission.title)
-else:
-    for submission in reddit.subreddit(secure_Random.choice(subreddits)).new(limit=5):
-        if str(redditFormatQuestions) in submission.title:
-            pass
-        pprint.pprint(submission.title)
+redditFormatQuestions = ['AskReddit',\
+                             'askreddit',\
+                             'reddit',\
+                             'Reddit',\
+                             'of Reddit',\
+                             'of reddit']
+
+
+
+class Gabfest():
+
+
+    def __init__(self, name, age, filters):
+
+        self.name = name
+        self.age = age
+        self.filters = filters
+
+
+    def questionGet(self):
+        if int(self.age) < 18:
+            for submission in reddit.subreddit(secure_Random.choice(self.filters)).new(limit=5):
+                if str(curseWords) and str(redditFormatQuestions) in submission.title:
+                    pass
+            print(self.name + ', Here is your question. ' + submission.title)
+
+        else:
+            for submission in reddit.subreddit(secure_Random.choice(self.filters)).new(limit=5):
+                if str(redditFormatQuestions) in submission.title:
+                    pass
+                print(self.name + ', Here is your question. ' + submission.title)
+
+        return True
+
+
+
+name = input("Please enter your name: ")
+age = input("Welcome " + name + ". Please enter your age: ")
+
+all_subreddits = ['AskMen','AskWomen','AskReddit','whowouldwin','explainlikeimfive']
+
+print("""Below are the subreddits you can recieve questions from.
+      You may use up to 5 subreddit\'s.
+      Please copy and paste the titles in the response below: """)
+
+for i in range(len(all_subreddits)):
+    print(all_subreddits[i])
+
+filters = []
+print('Enter a subreddit topics below. When finished, hit enter to continue the program.')
+while True:
+    print('Paste the subreddit here. Hit enter to leave the program: ')
+    reddit_titles = input()
+    if reddit_titles == '':
+        break
+    filters = filters + [reddit_titles]
+    if len(filters) == 5:
+        print("Subreddit's have been selected.")
+        break
+
+person1 = Gabfest(name, age, filters)
+person1.questionGet()
