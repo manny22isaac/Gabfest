e4be2e0701f964e4616a27693b281b6c8c696d87
manny22isaac <34252065+manny22isaac@users.noreply.github.com>
2019-08-28 17:14:08 -0400

    Documentation
    Added:
    - Comments to describe what each of the functions do. 
    - Removed an extra variable.
    -

--- askreddit.py
+++ askreddit.py
@@ -8,8 +8,6 @@ reddit = praw.Reddit(client_id='',
 print(reddit.read_only)
 
 
-secure_Random = random.SystemRandom()
-
 #--------------------------------------------------------#
     #selects a random subreddit from the subreddit list
 secure_Random = random.SystemRandom()
@@ -21,20 +19,14 @@ subreddits = ['AskReddit',\
                           'AskWomen']
 all_subreddits = []
 #--------------------------------------------------------#
-#this lists are used in the lines 52-67
 submissions = ['controversial',\
                       'new',\
                       'top',\
                       'gilded',\
                       'hot',\
                       'rising']
-curseWords = ['sex',\
-                  'NSFW',\
-                  'fuck',\
-                  'shit',\
-                  'ass',\
-                  'dick',\
-                  'cunt',]
+
+curseWords = ['CurseWords go here.']
 
 redditFormatQuestions = ['AskReddit',\
                              'askreddit',\
@@ -47,6 +39,7 @@ redditFormatQuestions = ['AskReddit',\
 
 class Gabfest():
 
+    #and the stored variables from in the class Gabfest sent to the init functions parameters using the self method to store the variables
 
     def __init__(self, name, age, filters):
 
@@ -54,6 +47,8 @@ class Gabfest():
         self.age = age
         self.filters = filters
 
+    # if a users age is below 18 it will pass any reddit post containing adult content. Otherwise it will return a question.
+    # the type of posts can be altered by the # of questions a user wants and what types of reddit titles
 
     def questionGet(self):
         if int(self.age) < 18:
@@ -71,6 +66,8 @@ class Gabfest():
         return True
 
 
+#allows a user to enter in their name age and subreddit topics.
+#The program will print out the titles of the available subreddit topics.
 
 name = input("Please enter your name: ")
 age = input("Welcome " + name + ". Please enter your age: ")
@@ -84,6 +81,10 @@ print("""Below are the subreddits you can recieve questions from.
 for i in range(len(all_subreddits)):
     print(all_subreddits[i])
 
+#allows a user to copy and paste the subreddit into the input
+#and fill up the list saved a the variable filters which will be used as a parameter
+#or it will end the program by leaving the input blank.
+
 filters = []
 print('Enter a subreddit topics below. When finished, hit enter to continue the program.')
 while True:
@@ -95,6 +96,8 @@ while True:
     if len(filters) == 5:
         print("Subreddit's have been selected.")
         break
+#sends the parameters (name, age, filters) provided by the user to the Class Gabfest as a local variable
+#takes the varible person1 and send the GET requests to reddit.
 
 person1 = Gabfest(name, age, filters)
 person1.questionGet()
